edf62e5677ca21d681bdfd8a597ee7c45cfc6c6f
mohandossKrishan <mohandoss28@gmail.com>
2016-12-07 12:07:40 +0530

    SumWithoutPlus

---
+++ SumWithoutPlus.java
@@ -0,0 +1,16 @@
+import java.io.*;  
+class SumWithoutPlus
+{  
+	public static void main(String args[])throws Exception
+	{  
+		int firstnumber ,secondnumber,sum;  
+		InputStreamReader r=new InputStreamReader(System.in);  
+		BufferedReader br=new BufferedReader(r);  
+		System.out.println("Enter the first Number :");	
+		firstnumber = Integer.parseInt(br.readLine());
+		System.out.println("Enter the Second Number :");	
+		secondnumber = Integer.parseInt(br.readLine());
+		sum = firstnumber - (-1 * secondnumber); 
+		System.out.println("The sum of " + firstnumber + " and " + secondnumber + " = " + sum);
+   	}  
+}  
